ea52a89b2a8933174dc54715055ebf9767217770
Petar Ivanov <peter.ivanov89@gmail.com>
2013-08-13 19:57:58 +0400

    feedzirra

--- Gemfile
+++ Gemfile
@@ -7,7 +7,8 @@ gem 'adsf'              # for viewing
 gem 'kramdown'          # for advanced markdown
 gem 'nokogiri'          # for parsing HTML
 gem 'feed-normalizer'   # for parsing feed
-gem 'truffle-hog'       # for finding rss
+#gem 'truffle-hog'       # for finding rss
 gem 'sanitize'          # sanitizing HTML
 gem 'rubypants'         # for beautiful typography in HTML
+gem 'feedzirra'         # for parsing and aggregating feeds
 
--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,10 +1,11 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
-require 'open-uri'
-require 'feed-normalizer'  # for parsing feeds
-require 'truffle-hog'      # for finding feeds
+#require 'open-uri'
+#require 'feed-normalizer'  # for parsing feeds
+#require 'truffle-hog'      # for finding feeds
 require 'sanitize'         # sanitizing HTML
+require 'feedzirra'        # for parsing and aggregating feeds
 
 include Nanoc::Helpers::Rendering
 include Utils 

447159f4dd733c40f2b767223b92f445317e96f4
Petar Ivanov <peter.ivanov89@gmail.com>
2013-08-20 22:57:42 +0400

    blogs page with all the authors

--- lib/helpers/utils.rb
+++ lib/helpers/utils.rb
@@ -8,7 +8,7 @@ module Utils
   def obfuscate_email(obf_email)
     orig_email = obf_email.gsub('.alabala.', '')
     AssertionError.assert{ orig_email.length < obf_email.length }    
-    return orig_email[0..2] + '<div id="antispam">answer42.195@runforever.com</div>' + orig_email[3..-1]
+    return orig_email[0..2] + '<div class="antispam">answer42.195@runforever.com</div>' + orig_email[3..-1]
   end
 
   def stripped_filename(item)
